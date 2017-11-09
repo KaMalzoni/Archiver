64db31143ca38a1de2a52a8a6dd3acc9a67fed71
KaMalzoni <karen.malzoni@hotmail.com>
2017-11-09 19:59:09 -0200

    mais código

--- Archiver/src/archiver/Archiver.java
+++ Archiver/src/archiver/Archiver.java
@@ -10,6 +10,7 @@ package archiver;
  * @author Karen
  */
 
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -24,9 +25,13 @@ public class Archiver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please insert the name of the file to be compressed");
+        String fname = sc.nextLine();
         try {
             FileOutputStream fos = new FileOutputStream("arqcompactado.zip");
             ZipOutputStream zos = new ZipOutputStream(fos);
+            addToZipFile(file1Name, zos);
         }
         catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -36,5 +41,25 @@ public class Archiver {
 	}
         // TODO code application logic here
     }
+    
+    public static void addToZipFile(String fileName, ZipOutputStream zos) throws FileNotFoundException, IOException {
+
+		System.out.println("Writing '" + fileName + "' to zip file");
+
+		File file = new File(fileName);
+		FileInputStream fis = new FileInputStream(file);
+		ZipEntry zipEntry = new ZipEntry(fileName);
+		zos.putNextEntry(zipEntry);
+
+		byte[] bytes = new byte[1024];
+		int length;
+		while ((length = fis.read(bytes)) >= 0) {
+			zos.write(bytes, 0, length);
+		}
+
+		zos.closeEntry();
+		fis.close();
+	}
+
     
 }
